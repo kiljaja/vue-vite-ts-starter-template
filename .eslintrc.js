078988a4c47623166d226a61f38b238f00facb75
Emilio Franco <ejfranco06@gmail.com>
2021-12-19 22:57:15 -0600

    refactor: reduce linting plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'airbnb-base',
-    'plugin:vue/vue3-essential',
-    'plugin:vue/vue3-recommended',
-    'plugin:vue/vue3-strongly-recommended',
-    'prettier',
-  ],
+  extends: ['airbnb-base', 'plugin:vue/vue3-recommended', 'prettier'],
   parserOptions: {
     ecmaVersion: 13,
     parser: '@typescript-eslint/parser',
