fef87f0f22535170ad45f643b8e279bdd50464df
Emilio Franco <ejfranco06@gmail.com>
2021-12-19 21:51:25 -0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+  import HelloWorld from './modules/HelloWorld.vue';
+</script>
+
+<template>
+  <HelloWorld msg="Hello World" />
+</template>
+
+<style></style>
---
+++ src/modules/HelloWorld.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+  import { ref } from 'vue';
+
+  defineProps<{ msg: string }>();
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<style></style>
